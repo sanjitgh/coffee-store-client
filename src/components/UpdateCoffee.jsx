90aaf20f6369616b5de70ddb3f3f7fd5e81c72ad
Sanjit Ghosh <sanjitkumarghosh0@gmail.com>
2024-11-27 23:25:44 +0600

    setup client site project frontend

---
+++ src/components/UpdateCoffee.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+            <h1>Update coffee</h1>
+        </div>
+    );
+};
+
+export default UpdateCoffee;
